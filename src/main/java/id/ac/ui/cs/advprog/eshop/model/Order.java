45ffc60a46468a004cfa8c9d24cc56d46763522f
ariana <ariana.nurlayla@ui.ac.id>
2024-03-06 19:14:32 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,10 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import enums.OrderStatus;
-import lombok.Setter;
-import lombok.Getter;
+
 import lombok.Builder;
-import java.util.*;
+import lombok.Getter;
+
+import java.util.List;
 
 @Builder
 @Getter
