64a3acafcf8f2143ce4d16603adb52a1f7b2b2b9
ariana <ariana.nurlayla@ui.ac.id>
2024-03-06 18:50:31 +0700

    [GREEN] Implement Order model

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -6,8 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import enums.OrderStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -53,14 +51,14 @@ public class OrderTest {
         assertEquals("13652556-012a-4c07-b546-54eb1396d79b", order.getId());
         assertEquals(1708560000L, order.getOrderTime());
         assertEquals("Safira Sudrajat", order.getAuthor());
-        assertEquals(OrderStatus.WAITING_PAYMENT.getValue(), order.getStatus());
+        assertEquals("WAITING_PAYMENT", order.getStatus());
     }
 
     @Test
     void testCreateOrderSuccessStatus() {
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
-                this.products, 1708560000L, "Safira Sudrajat", OrderStatus.SUCCESS.getValue());
-        assertEquals(OrderStatus.SUCCESS.getValue(), order.getStatus());
+                this.products, 1708560000L, "Safira Sudrajat", "SUCCESS");
+        assertEquals("SUCCESS", order.getStatus());
     }
 
     @Test
@@ -75,8 +73,8 @@ public class OrderTest {
     void testSetStatusToCancelled(){
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                 this.products, 1708560000L, "Safira Sudrajat");
-        order.setStatus(OrderStatus.CANCELLED.getValue());
-        assertEquals(OrderStatus.CANCELLED.getValue(), order.getStatus());
+        order.setStatus("CANCELLED");
+        assertEquals("CANCELLED", order.getStatus());
     }
 
     @Test
